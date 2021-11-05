2e58a98ca2dd132b4bd8dd16d7302e13d09d068e
IvanLotoff <71568721+IvanLotoff@users.noreply.github.com>
2021-11-05 14:26:17 +0300

    Update HelloController.java

--- src/main/java/no/kantega/springandreact/HelloController.java
+++ src/main/java/no/kantega/springandreact/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HelloController {
     @GetMapping("/api/hello")
     public String hello() {
